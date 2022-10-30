31e171122b54b6366fc540514898fdbef5d59003
David Garcia <David.GarOro@protonmail.com>
2022-10-29 23:28:26 -0600

    Added glfw and glad libraries, creating window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+  GLFWwindow* window;
+  if (!glfwInit())
+      return -1;
+  window = glfwCreateWindow(640,480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  while (!glfwWindowShouldClose(window))
+  {
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
   return 0;
 }
