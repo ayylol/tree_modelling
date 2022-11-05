d1684ed52c0098f640b7bb59732255d9ada501b2
David Garcia <David.GarOro@protonmail.com>
2022-11-05 16:49:11 -0600

    cleanup + basic 3d implemented

--- src/VAO.cpp
+++ src/VAO.cpp
@@ -2,10 +2,10 @@
 
 VAO::VAO() { glGenVertexArrays(1, &ID); }
 
-void VAO::linkVBO(VBO& VBO, GLuint layout)
+void VAO::link_attrib(VBO& VBO, GLuint layout, GLuint num_components, GLenum type, GLsizeiptr stride, void* offset)
 {
 	VBO.bind();
-	glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(layout, num_components, type, GL_FALSE, stride, offset);
 	glEnableVertexAttribArray(layout);
 	VBO.unbind();
 }
