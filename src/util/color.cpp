5fcbc38031d0ce30a305a99e91769a19d2f7ed86
David Garcia <David.GarOro@protonmail.com>
2022-12-11 15:15:30 -0700

    visualization improvement + finding parameters

--- src/util/color.cpp
+++ src/util/color.cpp
@@ -1,7 +1,7 @@
 #include "color.h"
 glm::vec3 random_color()
 {
-    const std::array<glm::vec3,7> palette {{
+    const std::vector<glm::vec3> palette {{
         glm::vec3(0.58,0,0.83),
         glm::vec3(0.29,0,0.51),
         glm::vec3(0,0,1),
@@ -10,5 +10,22 @@ glm::vec3 random_color()
         glm::vec3(1,0.5,0),
         glm::vec3(1,0,0)
     }};
+    return random_color(palette);
+}
+glm::vec3 random_brown()
+{
+    const std::vector<glm::vec3> palette {
+        glm::vec3(0.2,0.1,0),
+        glm::vec3(0.2,0.1,0),
+        glm::vec3(0.2,0.1,0),
+        glm::vec3(0.2,0.1,0),
+        glm::vec3(0.2,0.1,0),
+        glm::vec3(0.4,0.3,0.18),
+        glm::vec3(0.35,0.25,0.25),
+    };
+    return random_color(palette);
+}
+glm::vec3 random_color(const std::vector<glm::vec3>& palette)
+{
     return palette[rand()%palette.size()];
 }
