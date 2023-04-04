088a60efb3dcb4c8ba72c7f7199e00a5938beffd
David Garcia Orozco <David.GarOro@protonmail.com>
2023-04-04 15:09:43 -0600

    Parameter finding for implicits

--- src/const.h
+++ src/const.h
@@ -14,87 +14,125 @@
 */
 
 // Tree specific constants
-#define FOCUS glm::vec3(0, 1.5, 0)
-#define DISTANCE 5.f
+#define FOCUS glm::vec3(0,0.3,0)
+#define DISTANCE 2.f
+#define GRIDSCALEFACTOR 1.f
 // STRAND CONSTANTS
 #define SEGMENT_LENGTH .03f
 #define NUM_TRIALS 50
 #define MAX_ANGLE 90.f
-#define ALPHA 0.5f // Larger alpha makes distance more important vs angle
+#define ALPHA 0.5f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.005f
+#define BLOBINESS -5.f
+#define CUTOFF_VAL 0.5f
+#define REJECT_VAL 10.f
+#define SURFACE_VAL 1.f
+
+/*
+// For poplar
+#define FOCUS glm::vec3(0, 1.5, 0)
+#define DISTANCE 5.f
+#define GRIDSCALEFACTOR 0.6f
+// STRAND CONSTANTS
+#define SEGMENT_LENGTH .03f
+#define NUM_TRIALS 50
+#define MAX_ANGLE 90.f
+#define ALPHA 0.5f
 // IMPLICIT CONSTANTS
 #define RADIUS 0.005f
 #define BLOBINESS -1.f
 #define CUTOFF_VAL 0.05f
-//#define REJECT_VAL 1.f
 #define REJECT_VAL 2.f
-
-/*
-// For poplar
-#define FOCUS glm::vec3(0,1.5,0)
-#define DISTANCE 5.f
-#define DIMENSIONS glm::ivec3(500,500,500)
-#define SCALE .006f
-#define CENTER glm::vec3(0.2,1.5,0)
-// STRAND CONSTANTS
-#define SEGMENT_LENGTH .05f
-#define NUM_TRIALS 100
-#define MAX_ANGLE 200.f
-#define ALPHA 0.5f
+#define SURFACE_VAL 1.f
 
 // For malus baccata columnaris
-#define FOCUS glm::vec3(0,1.3,0)
+// THIS SHOWS LINE PRIMITIVE BULGING
+#define FOCUS glm::vec3(0, 1.5, 0)
 #define DISTANCE 5.f
-#define DIMENSIONS glm::ivec3(375,1000,375)
-#define SCALE .0022f
-#define CENTER glm::vec3(0,1.05,0)
+#define GRIDSCALEFACTOR 0.2f
 // STRAND CONSTANTS
-#define SEGMENT_LENGTH .01f
-#define NUM_TRIALS 100
-#define MAX_ANGLE 200.f
+#define SEGMENT_LENGTH .03f
+#define NUM_TRIALS 50
+#define MAX_ANGLE 90.f
 #define ALPHA 0.5f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.003f
+#define BLOBINESS -.4f
+#define CUTOFF_VAL 0.05f
+#define REJECT_VAL 5.f
+#define SURFACE_VAL 1.f
 
 // For nice tree
 #define FOCUS glm::vec3(0,0.3,0)
 #define DISTANCE 2.f
-#define DIMENSIONS glm::ivec3(650,390,650)
-#define SCALE .0014f
-#define CENTER glm::vec3(0,0.25,0)
+#define GRIDSCALEFACTOR 0.2f
 // STRAND CONSTANTS
 #define SEGMENT_LENGTH .003f
-#define NUM_TRIALS 50
+#define NUM_TRIALS 10
 #define MAX_ANGLE 200.f
 #define ALPHA 0.1f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.002f
+#define BLOBINESS -1.f
+#define CUTOFF_VAL 0.1f
+#define REJECT_VAL 0.6f
+#define SURFACE_VAL 1.f
 
 // For bush ra17
 #define FOCUS glm::vec3(0,0.5,0)
 #define DISTANCE 2.f
-#define DIMENSIONS glm::ivec3(500,480,480)
-#define SCALE .0022f
-#define CENTER glm::vec3(0.08,0.5,0)
+#define GRIDSCALEFACTOR 0.6f
 // STRAND CONSTANTS
 #define SEGMENT_LENGTH .001f
-#define NUM_TRIALS 30
+#define NUM_TRIALS 10
 #define MAX_ANGLE 200.f
 #define ALPHA 0.5f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.003f
+#define BLOBINESS -1.f
+#define CUTOFF_VAL 0.01f
+#define REJECT_VAL 0.6f
+#define SURFACE_VAL 1.f
 
 // For tall ellipsoid tree
 #define FOCUS glm::vec3(0,0.8,0)
 #define DISTANCE 3.f
-#define DIMENSIONS glm::ivec3(400,500,400)
-#define SCALE .0035f
-#define CENTER glm::vec3(0,0.84,0)
+#define GRIDSCALEFACTOR 0.6f
+// STRAND CONSTANTS
+#define SEGMENT_LENGTH .03f
+#define NUM_TRIALS 50
+#define MAX_ANGLE 90.f
+#define ALPHA 0.5f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.003f
+#define BLOBINESS -0.5f
+#define CUTOFF_VAL 0.5f
+#define REJECT_VAL 3.f
+#define SURFACE_VAL 1.f
 
 // For oak 4
 #define FOCUS glm::vec3(0,0.2,0)
 #define DISTANCE 2.7f
-#define DIMENSIONS glm::ivec3(580,340,580)
-#define SCALE .0026f
-#define CENTER glm::vec3(0.03,0.4,0)
 
 // For gnarled
 #define FOCUS glm::vec3(0,0.3,0)
 #define DISTANCE 2.f
-#define DIMENSIONS glm::ivec3(650,390,650)
-#define SCALE .002f
-#define CENTER glm::vec3(0,0.32,0)
+
+
+// For Irregular Crown
+#define FOCUS glm::vec3(0,0.3,0)
+#define DISTANCE 2.f
+#define GRIDSCALEFACTOR 1.f
+// STRAND CONSTANTS
+#define SEGMENT_LENGTH .03f
+#define NUM_TRIALS 50
+#define MAX_ANGLE 90.f
+#define ALPHA 0.5f
+// IMPLICIT CONSTANTS
+#define RADIUS 0.005f
+#define BLOBINESS -5.f
+#define CUTOFF_VAL 0.5f
+#define REJECT_VAL 10.f
+#define SURFACE_VAL 1.f
 */
